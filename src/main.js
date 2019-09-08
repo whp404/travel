fa49359b5425e07f3539927f7b46b011573bc52e
whp404 <whp404@sina.com>
2019-09-08 17:06:41 +0800

    主页搜索框

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/style/reset.css'
+//解决一像素边框问题
+import './assets/style/border.css'
+//解决移动端的点击延迟问题
+import  fastClick from 'fastclick'
+import './assets/style/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
